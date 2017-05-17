67f4b4c102d5fa54f8b581e55e79dc1f447163be
Charles Dunn <dunnc1551@gmail.com>
2017-05-16 21:37:00 -0600

    Update seed file to populate db with location and park data.

--- db/seeds/dev/locations.js
+++ db/seeds/dev/locations.js
@@ -1,25 +1,28 @@
 const data = require('../../../data_cleaner.js');
-console.log(data);
 
 exports.seed = function(knex, Promise) {
-  return knex('parks').del()
-    .then(() => knex('locations').del())
-    .then(() => {
-      return Promise.all([
-        data.forEach(place => {
-          knex('locations').insert({
-            city: place.city
-          }, 'id')
-          .then(place => {
-            return knex('parks').insert(
-              { city_id: location[0],
-                name: place.activities[0].name,
-                activity_type: place.activities[0].activity_type_name,
-                activity_description: place.activities[0].description,
-              }
-            )
-          })
-        })
-      ])
-    });
+  var localePromises = []
+  data.forEach(location => {
+    localePromises.push(createLocation(knex, location));
+  });
+
+  return Promise.all(localePromises);
+}
+
+const createLocation = (knex, location) => {
+  return knex.table('locations')
+    .returning('id')
+    .insert(
+      { city: location.city}
+    )
+    .then(cityIDs => {
+      return knex('parks')
+        .insert(
+        { city_id: cityIDs[0],
+          name: location.activities[0].name,
+          activity_type: location.activities[0].activity_type_name,
+          activity_description: location.activities[0].description,
+        }
+      );
+    })
 }
