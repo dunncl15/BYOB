70871b1bd0bf909e8e8f9e517a6b7a78c4c033ed
Charles Dunn <dunnc1551@gmail.com>
2017-05-17 18:47:32 -0600

    Add POST routes for locations and parks. Set up JWT's for both POST routes.

--- server.js
+++ server.js
@@ -1,29 +1,62 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const config = require('dotenv').config().parsed;
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.set('port', process.env.PORT || 3000)
+app.set('secretKey', config.CLIENT_SECRET);
+let token = jwt.sign('token', app.get('secretKey'));
+console.log(token);
+
+if (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD) {
+  throw 'Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file'
+}
+
+const checkAuth = (request, response, next) => {
+  const token = request.body.token ||
+                request.param('token') ||
+                request.headers['authorization'];
+
+  if (token) {
+    jwt.verify(token, app.get('secretKey'), (error, decoded) => {
+    if (error) {
+      return response.status(403).send({
+        success: false,
+        message: 'Invalid authorization token.'
+      });
+    } else {
+      request.decoded = decoded;
+      next();
+      }
+    })
+  } else {
+    return response.status(403).send({
+      success: false,
+      message: 'You must be authorized to hit this endpoint'
+    });
+  }
+}
+
+
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
   response.status(404).send({ error: 'Route not found.' })
 })
 
 app.get('/api/v1/locations', (request, response) => {
-  const { url } = request;
   database('locations').select()
   .then(locations => {
-    if (url !== '/api/v1/locations/') {
-      response.status(400).send({ error: 'Invalid request url.' })
-    } else {
-      response.status(200).json(locations);
-    }
+    response.status(200).json(locations);
   })
   .catch(error => response.status(500).send({ error: error }));
 })
@@ -74,6 +107,23 @@ app.get('/api/v1/locations/:id/parks', (request, response) => {
   .catch(error => response.status(500).send({ error: error }));
 })
 
+app.post('/api/v1/locations', checkAuth, (request, response) => {
+  const location = request.body;
+  database('locations').insert(location, 'id')
+  .then(location => {
+    response.status(201).json({ id: location[0] })
+  })
+  .catch(error => response.status(500).send({ error: error }));
+})
+
+app.post('/api/v1/parks', checkAuth, (request, response) => {
+  const park = request.body;
+  database('parks').insert(park, 'id')
+  .then(park => {
+    response.status(201).json({ id: park[0] })
+  })
+  .catch(error => response.status(500).send({ error: error }));
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Server running on port ${app.get('port')}.`);
