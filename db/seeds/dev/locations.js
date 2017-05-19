bbad60088bd1cc96ea6b2072e100c49cff5d45b8
Charles Dunn <dunnc1551@gmail.com>
2017-05-19 15:13:31 -0600

    Move Promise.all inside return block.

--- db/seeds/dev/locations.js
+++ db/seeds/dev/locations.js
@@ -1,31 +1,30 @@
 const data = require('../../../helpers/data_cleaner.js');
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
+  const createLocation = (knex, location) => {
+    return knex.table('locations')
+      .returning('id')
+      .insert(
+        { city: location.city }
+      )
+      .then((cityIDs) => {
+        return knex('parks')
+          .insert(
+          { city_id: cityIDs[0],
+            name: location.activities[0].name,
+            activity_type: location.activities[0].activity_type_name,
+            activity_description: location.activities[0].description,
+          }
+        );
+      });
+  };
   return knex('parks').del()
     .then(() => knex('locations').del())
     .then(() => {
-      let localePromises = [];
-      data.forEach(location => {
+      const localePromises = [];
+      data.forEach((location) => {
         localePromises.push(createLocation(knex, location));
       });
-    })
-    return Promise.all(localePromises);
-}
-
-const createLocation = (knex, location) => {
-  return knex.table('locations')
-    .returning('id')
-    .insert(
-      { city: location.city}
-    )
-    .then(cityIDs => {
-      return knex('parks')
-        .insert(
-        { city_id: cityIDs[0],
-          name: location.activities[0].name,
-          activity_type: location.activities[0].activity_type_name,
-          activity_description: location.activities[0].description,
-        }
-      );
-    })
-}
+      return Promise.all(localePromises);
+    });
+};
